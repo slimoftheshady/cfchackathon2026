6e8c28325ba54e75b1ed956bd4c33266aaab81f9
saltyypringle <bookreader667@gmail.com>
2026-09-06 10:07:15 +0800

    getting demo ready

--- app.py
+++ app.py
@@ -7293,4 +7293,4 @@ def complete_manual_quest(classroom_id, quest_id):
 init_db()
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=False)
